e404d50b669d6e55a8b95d9bfab268574f8142a1
Pietro Passarelli <pietro.passarelli@wsj.com>
2020-04-25 02:37:50 -0400

    smalle tweak

--- src/util/export-adapters/slate-to-dpe/index.js
+++ src/util/export-adapters/slate-to-dpe/index.js
@@ -1,10 +1,29 @@
 // import slateToText from '../txt';
 import { Node } from 'slate';
-
 // importing this way, coz it runs as client side code, and the module, align-diarized-text index.js contains and import to a
 // helper module to generate html view, that contains and fs, and it breaks storybook webpack.
 // TODO: refactor in ` align-diarized-text` so that it can work outside node only, but also in browser, without workaround
 const alignDiraizedText = require('../../../../node_modules/align-diarized-text/src/add-timecodes-to-quotes');
+
+// TODO: this function needs to be brough into alignDiraizedText
+// and applied to paragraphs - to avoid boundaries overlapp
+function adjustTimecodesBoundaries(words) {
+    return words.map((word, index, arr) => {
+        // excluding first element
+        if (index != 0 ) {
+        const previousWord = arr[index - 1];
+        const currentWord = word;
+        if (previousWord.end > currentWord.start) {
+            word.start = previousWord.end;
+        }
+    
+        return word;
+        }
+    
+        return word;
+    });
+}
+
 const prepSlateParagraphForAlignement = (slateData)=>{
     const result = [];
     slateData.forEach((el, index)=>{
@@ -19,7 +38,7 @@ const prepSlateParagraphForAlignement = (slateData)=>{
     return result;
 }
 const converSlateToDpe = (data,sttJson)=>{
-    const  linesWithSpeaker = prepSlateParagraphForAlignement(data);
+    const linesWithSpeaker = prepSlateParagraphForAlignement(data);
     const res = alignDiraizedText(linesWithSpeaker, sttJson);
     const words =  res.map((paragraph)=>{
         return paragraph.words
@@ -32,26 +51,10 @@ const converSlateToDpe = (data,sttJson)=>{
             id: paragraph.id
         }
     }).flat()
-
-      // TODO: this function needs to be brough into alignDiraizedText
-    // and applied to paragraphs - to avoid boundaries overlapp
-    function adjustTimecodesBoundaries(words) {
-        return words.map((word, index, arr) => {
-          // excluding first element
-          if (index != 0 ) {
-            const previousWord = arr[index - 1];
-            const currentWord = word;
-            if (previousWord.end > currentWord.start) {
-              word.start = previousWord.end;
-            }
-      
-            return word;
-          }
-      
-          return word;
-        });
-      }
-      const paragraphsWithAdjustedBoundaries = adjustTimecodesBoundaries(paragraphs)
+    // without adjusting the paragraph boundaries, can't go round trip
+    // back to slate, coz it's not able to reliably interpolate 
+    // words and speaker again 
+    const paragraphsWithAdjustedBoundaries = adjustTimecodesBoundaries(paragraphs)
    return {words, paragraphs:paragraphsWithAdjustedBoundaries};
 }
 
