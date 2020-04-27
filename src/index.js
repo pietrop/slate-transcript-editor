5d4bb0492d17f6cfa913170a97b8e91aca1cac2d
Pietro Passarelli <pietro.passarelli@wsj.com>
2020-04-27 18:58:58 -0400

    0.0.6

--- src/index.js
+++ src/index.js
@@ -4,10 +4,9 @@ import {
   timecodeToSeconds,
   shortTimecode
 } from './util/timecode-converter/index.js';
-
 import convertDpeToSlate from './util/dpe-to-slate/index.js';
 import converSlateToDpe from './util/export-adapters/slate-to-dpe/index.js';
-import slateToText from './uitl/export-adapters/txt';
+import slateToText from './util/export-adapters/txt';
 
 export default SlateTranscriptEditor;
 
