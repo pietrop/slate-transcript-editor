713babe59043803f136cb8c9c66fe3f1939b73dd
Pietro Passarelli <pietro.passarelli@wsj.com>
2021-01-25 20:49:54 -0500

    added convertSlateToDpeAsync to exports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import SlateTranscriptEditor from './components/index.js';
 import { secondsToTimecode, timecodeToSeconds, shortTimecode } from './util/timecode-converter/index.js';
 import convertDpeToSlate from './util/dpe-to-slate/index.js';
-import converSlateToDpe from './util/export-adapters/slate-to-dpe/index.js';
+import converSlateToDpe, { convertSlateToDpeAsync } from './util/export-adapters/slate-to-dpe/index.js';
 import slateToText from './util/export-adapters/txt';
 
 export default SlateTranscriptEditor;
 
-export { SlateTranscriptEditor, secondsToTimecode, timecodeToSeconds, shortTimecode, convertDpeToSlate, converSlateToDpe, slateToText };
+export {
+  SlateTranscriptEditor,
+  secondsToTimecode,
+  timecodeToSeconds,
+  shortTimecode,
+  convertDpeToSlate,
+  converSlateToDpe,
+  convertSlateToDpeAsync,
+  slateToText,
+};
