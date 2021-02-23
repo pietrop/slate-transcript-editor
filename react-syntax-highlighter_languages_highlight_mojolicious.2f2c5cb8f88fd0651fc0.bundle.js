62bead64671b77dc693158c17f4a9071e754e862
Pietro Passarelli <pietro.passarelli@wsj.com>
2021-02-23 00:28:56 -0500

    Updates

---
+++ react-syntax-highlighter_languages_highlight_mojolicious.2f2c5cb8f88fd0651fc0.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[104],{1569:function(module,exports){module.exports=function(hljs){return{subLanguage:"xml",contains:[{className:"meta",begin:"^__(END|DATA)__$"},{begin:"^\\s*%{1,2}={0,2}",end:"$",subLanguage:"perl"},{begin:"<%{1,2}={0,2}",end:"={0,1}%>",subLanguage:"perl",excludeBegin:!0,excludeEnd:!0}]}}}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_highlight_mojolicious.2f2c5cb8f88fd0651fc0.bundle.js.map
---
+++ react-syntax-highlighter_languages_highlight_vbscriptHtml.2f2c5cb8f88fd0651fc0.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[170],{1635:function(module,exports){module.exports=function(hljs){return{subLanguage:"xml",contains:[{begin:"<%",end:"%>",subLanguage:"vbscript"}]}}}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_highlight_vbscriptHtml.2f2c5cb8f88fd0651fc0.bundle.js.map
