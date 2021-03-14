49a6065e22bd60061d0a3c12e5a997e716e2cfd4
Joseph Shearer <joseph@josephshearer.net>
2021-03-14 15:33:31 -0400

    fix: OHMS export
    Since alignment is running on paragraphs instead of the whole document,
    the code to inject timecodes inline was starting from the beginning
    of each paragraph, instead of continuing from where it left off in the
    previous paragraph.

--- src/util/insert-timecodes-in-line-in-words-list/index.js
+++ src/util/insert-timecodes-in-line-in-words-list/index.js
@@ -8,31 +8,42 @@ import { shortTimecode } from '../timecode-converter';
  * `slate-transcript-editor` OHMS export option exports the word part.
  * Thi functions organises the words to add timecodes intervals at the required times.
  */
+
 import convertWordsToText from '../convert-words-to-text';
 
-const insertTimecodesInlineinWordsList = ({ intervalSeconds = 30, words }) => {
+const insertTimecodesInlineinWordsList = ({ intervalSeconds = 30, words, lastInsertTime = 0 }) => {
   const tmpWords = JSON.parse(JSON.stringify(words));
-  let lastInsertTime = 0;
-
   const sortedWords = tmpWords.sort((a, b) => a.start - b.start);
-
   let newWords = [];
+
   for (const word of sortedWords) {
     if (word.start - lastInsertTime > intervalSeconds) {
       lastInsertTime = Math.floor(word.start / intervalSeconds) * intervalSeconds;
       const timecode = shortTimecode(lastInsertTime);
-      newWords.push({ start: word.start, end: word.start + (word.end - word.start) / 2, text: `[${timecode}]` });
+      newWords.push({
+        start: word.start,
+        end: word.start + (word.end - word.start) / 2,
+        text: `[${timecode}]`,
+      });
       word.start = word.start + (word.end - word.start) / 2;
     }
+
     newWords.push(word);
   }
-  return newWords;
+
+  return [newWords, lastInsertTime];
 };
 
 const insertTimecodesInLineInSlateJs = (slateValue) => {
+  let lastInsertTime = 0;
   return slateValue.map((block) => {
     const newBlock = JSON.parse(JSON.stringify(block));
-    newBlock.children[0].words = insertTimecodesInlineinWordsList({ words: newBlock.children[0].words });
+    const [newWords, lastInsert] = insertTimecodesInlineinWordsList({
+      words: newBlock.children[0].words,
+      lastInsertTime,
+    });
+    lastInsertTime = lastInsert;
+    newBlock.children[0].words = newWords;
     newBlock.children[0].text = convertWordsToText(newBlock.children[0].words);
     return newBlock;
   });
