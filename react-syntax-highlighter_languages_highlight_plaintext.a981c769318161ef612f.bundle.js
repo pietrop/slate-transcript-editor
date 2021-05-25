e5db3d2d73b3b64aea73570cf9f081d851209ea7
Pietro Passarelli <pietro.passarelli@wsj.com>
2021-05-24 23:01:32 -0400

    Updates

--- react-syntax-highlighter_languages_highlight_plaintext.a981c769318161ef612f.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[121],{1591:function(module,exports){module.exports=function(hljs){return{disableAutodetect:!0}}}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_highlight_plaintext.a981c769318161ef612f.bundle.js.map
--- react-syntax-highlighter_languages_highlight_vbscriptHtml.a981c769318161ef612f.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[170],{1640:function(module,exports){module.exports=function(hljs){return{subLanguage:"xml",contains:[{begin:"<%",end:"%>",subLanguage:"vbscript"}]}}}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_highlight_vbscriptHtml.a981c769318161ef612f.bundle.js.map
